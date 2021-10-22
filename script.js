6b638fd517066f95f9bfa91209735072a36fc4ce
johmert <johmert@users.noreply.github.com>
2021-10-21 23:24:46 -0700

    whisper() now gives a random preposition for a truly MadLibs feel

--- script.js
+++ script.js
@@ -3,9 +3,11 @@ return {
     name: name,
     relatedNouns: relatedNouns,
     whisper(){
+        const codePrepositions = ['in', 'behind', 'in front of', 'under', 'on top of', 'deep within', 'over'];
         let codename = this.name;
         let codeLocation = this.relatedNouns[Math.floor(Math.random() * this.relatedNouns.length)];
-        return `The ${codename} is in the ${codeLocation}`;
+        let codePrep = codePrepositions[Math.floor(Math.random() * codePrepositions.length)];
+        return `The ${codename} is ${codePrep} the ${codeLocation}`;
     }
 }
 };
@@ -19,7 +21,7 @@ const displayCodePhrase = () => {
 
 const foxRelatedLocations = ['henhouse', 'foxhole', 'rabbit\'s den', 'yiffing corner'];
 const eagleRelatedLocations = ['nest', 'trees', 'sky', 'river', 'rich yuppie\'s cabin'];
-const mooseRelatedLocations = ['baseball field', 'forest', 'water', 'taxidermist\'s hands'];
+const mooseRelatedLocations = ['baseball field', 'forest', 'water', 'taxidermy center'];
 
 codePhrases.push(codePhraseFactory('fox', foxRelatedLocations));
 codePhrases.push(codePhraseFactory('eagle', eagleRelatedLocations));
