0e3e3d6aeeed0035f638809db35d2ee1c4a7c0b2
johmert <johmert@users.noreply.github.com>
2021-10-21 22:51:11 -0700

    displayCodePhrase function

--- script.js
+++ script.js
@@ -10,7 +10,24 @@ return {
 }
 };
 
-// Testing the Factory
+/* Testing the Factory
 let relatedArray = ['henhouse', 'foxhole', 'rabbit\'s den'];
 let test = codePhraseFactory ('fox', relatedArray);
-console.log(test.whisper())
+console.log(test.whisper()) */
+
+const codePhrases = [];
+
+const displayCodePhrase = () => {
+    let index = Math.floor(Math.random() * codePhrases.length);
+    return codePhrases[index].whisper();
+}
+
+const foxRelatedLocations = ['henhouse', 'foxhole', 'rabbit\'s den', 'yiffing corner'];
+const eagleRelatedLocations = ['nest', 'trees', 'sky', 'river', 'rich yuppie\'s cabin'];
+const mooseRelatedLocations = ['baseball field', 'forest', 'water', 'taxidermist\'s hands'];
+
+codePhrases.push(codePhraseFactory('fox', foxRelatedLocations));
+codePhrases.push(codePhraseFactory('eagle', eagleRelatedLocations));
+codePhrases.push(codePhraseFactory('moose', mooseRelatedLocations));
+
+console.log(displayCodePhrase());
